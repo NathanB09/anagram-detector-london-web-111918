347980b48802b8354256549a6b704248d75b3d1f
NathanB09 <nathanbell09@gmail.com>
2018-11-24 16:06:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.each_with_object([]) do |check, anagrams|
+      anagrams << check if word.split("").sort.join == check.split("").sort.join
+    end
+  end
+
+end
+
+# a = Anagram.new("listen")
+
+# Pry.start
